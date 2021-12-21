edac4d0219816fff40f43c1ee33ef57230480771
Jadmin3 <admin@xxc520.cn>
2021-12-21 22:04:36 +0800

    feat:task

--- admin/src/main/java/com/xxc/base/admin/moudle/task/TaskService.java
+++ admin/src/main/java/com/xxc/base/admin/moudle/task/TaskService.java
@@ -1,4 +1,14 @@
 package com.xxc.base.admin.moudle.task;
 
+import org.springframework.stereotype.Service;
+
+/**
+ * 运行一些常跑的任务
+ * @author Jadmin
+ */
+@Service
 public class TaskService {
+
+
+
 }
